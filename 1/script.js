9fff2dd40926d4eeee10a690d5078b9cfba1e85e
Roscoe <elysiaroscoe@gmail.com>
2021-10-18 13:03:42 -0700

    final

--- 1/script.js
+++ 1/script.js
@@ -1,9 +1,7 @@
 //Print odds 1-20
 //Using a loop write code that will console.log all of the odd values from 1 up to 20.
-if(var i=1; i<=20; i++){
-    var odd = i % 2 == 0
-    if odd = true{
-        console.log (i)
+for(var i=1; i<=20; i++){
+    if(i % 2 == 1){
+        console.log (i);
     }
 }
-
---
+++ 3/script.js
@@ -0,0 +1,8 @@
+//Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
+
+const num = [4, 2.5, 1, -0.5, -2, -3.5];
+
+for(var i=0; i<num.length; i++){
+
+    console.log(num[i]);
+}
